ed9aba414fa56969d45131578a86d3c54d07f6d1
ArvinH16 <arvin@hakakian.me>
2023-09-14 18:21:06 -0700

    switched from boostrap to css only

--- src/home.js
+++ src/home.js
@@ -1,14 +1,8 @@
-document.addEventListener('DOMContentLoaded', function() {
-    const navbarToggle = document.getElementById('navbarToggle');
-    const navbarNav = document.getElementById('navbarNav');
-
-    navbarToggle.addEventListener('click', function() {
-        if (navbarNav.classList.contains('show')) {
-            navbarNav.style.maxHeight = null; // Reset max-height
-            navbarNav.classList.remove('show');
-        } else {
-            navbarNav.style.maxHeight = navbarNav.scrollHeight + "px"; // Set max-height to actual height
-            navbarNav.classList.add('show');
-        }
-    });
+document.getElementById('navbarToggle').addEventListener('click', () => {
+    const navbar = document.getElementById('navbar');
+    if (navbar.classList.contains('expanded')) {
+        navbar.classList.remove('expanded');
+    } else {
+        navbar.classList.add('expanded');
+    }
 });
