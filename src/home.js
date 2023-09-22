420a54acf52eafe8935c911df59e67bc3c99c2c1
ArvinH16 <arvin@hakakian.me>
2023-09-21 18:49:00 -0700

    navbar update

--- src/home.js
+++ src/home.js
@@ -1,8 +1,4 @@
 document.getElementById('navbarToggle').addEventListener('click', () => {
     const navbar = document.getElementById('navbar');
-    if (navbar.classList.contains('expanded')) {
-        navbar.classList.remove('expanded');
-    } else {
-        navbar.classList.add('expanded');
-    }
+    navbar.classList.toggle('expanded');
 });
